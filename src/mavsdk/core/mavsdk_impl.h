49ed0f03804dfd97a3b96d36b24e365247407d30
thomas.an <xueshen.an@aeroratech.com>
2025-09-03 11:22:16 +0800

    revert headbeat time to 1s

--- src/mavsdk/core/mavsdk_impl.h
+++ src/mavsdk/core/mavsdk_impl.h
@@ -180,7 +180,7 @@ private:
 
     std::atomic<double> _timeout_s{Mavsdk::DEFAULT_TIMEOUT_S};
 
-    static constexpr double HEARTBEAT_SEND_INTERVAL_S = 0.08;
+    static constexpr double HEARTBEAT_SEND_INTERVAL_S = 1.0;
     void* _heartbeat_send_cookie{nullptr};
 
     std::atomic<bool> _should_exit = {false};
